547c07454bd8b43380872021adad6c478b05ad7b
ogata-k <ogtkzk@yahoo.co.jp>
2018-04-06 18:58:44 +0900

    make print command

--- src/FileStructure.hs
+++ src/FileStructure.hs
@@ -1,13 +1,21 @@
 module FileStructure(
-    FileStructure(..),
-    toString
+      FileStructure(..)
+    , toString
+    , convert
 )where
+
+import System.Directory
+import System.FilePath.Windows ((</>), takeFileName)
+import Control.Applicative ((<$>))
+import Control.Monad (join)
 
 data FileStructure  = Dir String [FileStructure]
                     | File String
                     deriving (Show)
 
 toString' :: Int -> FileStructure -> String
+toString' _ (File "") = ""
+toString' _ (Dir "" _) = ""
 toString' 0 (File s) = s ++ "\n"
 toString' 0 (Dir s fs) = s ++ "\n" ++ (concatMap (toString' 1) fs)
 toString' n (File s) = (concat (replicate (n - 1) "|   ")) ++ "|- " ++ s ++ "\n"
@@ -15,3 +23,31 @@ toString' n (Dir s fs) = (concat (replicate (n - 1) "|   ")) ++ "|- " ++ s ++ "\
 
 toString :: FileStructure -> String
 toString = toString' 0
+
+convert :: FilePath -> IO FileStructure
+convert fp = do
+    file <- doesFileExist fp
+    direc <- doesDirectoryExist fp
+    changeHead <$> convert' file direc fp
+    where
+        takeFileName' :: FileStructure -> FileStructure
+        takeFileName' (File s) = File $ takeFileName s
+        takeFileName' (Dir s fs) = Dir (takeFileName s) $ map takeFileName' fs
+
+        convert' :: Bool -> Bool -> FilePath -> IO FileStructure
+        convert' file direc fp
+            | file = return (takeFileName' $ File fp)
+            | direc = do
+                        lst <- listDirectory fp
+                        converted' <- mapM (convert . (</>) fp) lst
+                        let converted = map takeFileName' converted'
+                        return $ Dir fp converted
+            | otherwise  = return (File "")
+
+        changeHead :: FileStructure -> FileStructure
+        changeHead (File "") = File ""
+        changeHead (File s) = File fp 
+        changeHead (Dir s fs) = Dir fp fs
+
+printFileStructure :: FilePath -> IO ()
+printFileStructure fp = join $ putStr <$> toString <$> (convert fp)
