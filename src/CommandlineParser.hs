81064d9eb19ccfaabed26dcae0f1a713349a5737
ogata-k <ogtkzk@yahoo.co.jp>
2018-04-07 22:05:30 +0900

    make mainParser

--- src/CommandlineParser.hs
+++ src/CommandlineParser.hs
@@ -7,4 +7,4 @@ import Options.Declarative
 mainParser  :: Arg "targetDir" FilePath
             -> Flag "f" '["to"] "FileName" "target-file to write in targetFile.txt" (Def "result" String)
             -> Cmd "this program is to get file structure program" ()
-mainParser = undefined
+mainParser fp target = liftIO $ printFileStructure $ get fp
